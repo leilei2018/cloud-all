ac5aa26c5d0c1afebf8dd7ec96f40a90c01b7e80
hepan <he702170585@163.com>
2020-06-15 11:45:26 +0800

    develop分支修改一个merge点

--- eureka-server/src/main/java/com/fd/eurekaserver/EurekaServerBootstrapApplication.java
+++ eureka-server/src/main/java/com/fd/eurekaserver/EurekaServerBootstrapApplication.java
@@ -31,7 +31,7 @@ public class EurekaServerBootstrapApplication {
 
     @PostMapping("/qq/{id}")
     public String weq( @RequestBody(required = false) LogVo lg){
-        System.out.println("master");
+        System.out.println("develop");
         return "qwe";
     }
 
